bff2eeb3f407c92f0b06720202b2b92a439c3314
thehearsty14 <thehearsty14@hotmail.com>
2015-12-15 12:04:08 +0200

    frek.m dosyası olusturuldu.

--- frek.m
+++ frek.m
@@ -1,5 +1,5 @@
-function [A]=frek(nota,oktav)                                               %frek adýnda bir fonksiyon olusturduk   nota,oktav argümanlarý
+function [A]=frek(nota,oktav)                                                %frek adýnda bir fonksiyon olusturduk   nota,oktav argümanlarý
 notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %notalarý tanýmladýk
 x=strcmp(notalar,nota);                                                     %notaya dizilerde eriþme iþlemi
 y=find(x,1);                                                                %indeks e eriþim
-A=round(16.35*(2^oktav)*(2^((y-1)/12)));                                    %sayýyý en yakýn deðere yuvarladýk yuvarlama kodu
+A=round(16.35*(2^oktav)*(2^((y-1)/12)));                                    %sayýyý en yakýn deðere yuvarladýk yuvarlama kodu.
