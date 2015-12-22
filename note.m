735f6444e6e25c956b1b1361e32095e6c41b5e11
thehearsty14 <thehearsty14@hotmail.com>
2015-12-22 12:33:31 +0200

    note.m düzenlendi

--- note.m
+++ note.m
@@ -1,4 +1,31 @@
-function [a,b] = note(f,vurus)                                              %fonksiyon tanýmladýk argümanlarý f ve vuruþ olan
+function [x,t] = note(f,vurus)                                              %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
+
 fs=8192;                                                                    %örnekleme frekansý
-b = 0:1/fs:(vurus-1/fs);                                                    %b taným
-a = sin(2*pi*f*b);                                                          %sinüs 
+
+t = 0:1/fs:vurus;                                                           %0 dan dur a kadar 1/(100*ff) kadar artýyor
+
+elemanS = length(tt);                                                       %% Eleman sayýsý
+
+
+    
+attack = linspace(0,1.5,(elemanS*2/8));                                     
+
+
+    
+decay = linspace(1.5-(0.5/(elemanS/8)),1,(elemanS/8));
+
+
+    
+sustain = ones(1,(elemanS/2));
+
+
+
+relase = linspace(1-(1/(elemanS/8)),0,(elemanS/8));
+
+
+    
+zarf = [attack, decay, sustain, relase];
+
+
+
+x = zarf .* sin(2*pi*f*t + zarf);                                           %% zarflý sinüs
