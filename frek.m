3442d1956d7fa39491a6283e535181994bc7cda6
thehearsty14 <thehearsty14@hotmail.com>
2015-12-29 12:19:14 +0200

    frek.m düzenlendi

--- frek.m
+++ frek.m
@@ -1,25 +1,32 @@
-function [frekans]=frek(nota,oktav)                                         %Girilen nota ve oktava göre frekans döndürülüyor.
-notalar={'Do','C#','Re','Eb','Mi','Fa','F#','Sol','G#','La','Bb','Si'};     %Notalarýmýz bir dizi içerisinde yazýldý.
-n=strcmp(notalar,nota);                                                     %Fonksiyona gönderdiðimiz notayý dizi içerisinde aratýr ve buduðu indekse 1 atar
-m=find(n,1);                                                                %n dizisinde 1 deðerinin indeksini döndürür.
+function [frekans]=frek(nota,oktav,oktavdegeri)                             %frekans olusturduk
+notalar={'Do','Dod','Re','Mib','Mi','Fa','Fad','Sol','Sold','La','Sib','Si','Sus'}; %notalardan olusan bir dizi
+n=strcmp(notalar,nota);                                                     
+m=find(n,1);                                                                %n deki 1 deðerini buluyor.
+        if (nargin==0)                                                      %nargine yazýlan deðer 0 girilmezse kodlarý çalýstýr komutu
+                 disp ('veri girisi yap');                        
+        elseif (nargin==1)                                                  %tek deðiþken gönderilme durumunda
+                 if (m==13)                                                 
+                    frekans=0;                                              %sus deðeri 0
+                 else                                                       
+                    frekans=16.35*(2^(4))*(2^((m-1)/12));                   %frekans hesaplar
+                 end                                                        %sus notasý bitirme
+        elseif (nargin==2)                                                  
+                 if (m==13)                                                 
+                    frekans=0;                                              %frekansý 0 yap
+                 else                                                       
+                    frekans=16.35*(2^(oktav))*(2^((m-1)/12));               %oktav fonka göre frekans üretir
+                 end
+        else                                                                
+                 if (m==13)                                                 %nota degeri sus ise 
+                     frekans=0;                                             %frekansý 0 yap
+                 else                                                       
+                     frekans=16.35*(2^(oktav+oktavdegeri))*(2^((m-1)/12));  %frekans hesaplamasý yapar
+                 end
 
 
-if (nargin==0)
-    
-    disp ('Lütfen veri giriþi yapýnýz: ')
-    
-else if (nargin==1)
-        if(m==13)
-            frekans=0;
-        else
-           frekans=floor(16.35*(2^4)*(2^((m-1)/12)));                       % Frekans fonksiyonu oluþturuldu. 
-           
-        end
-    else 
-        if (m==13) 
-            frekans=0;
-        else frekans=floor(16.35*(2^oktav)*(2^((m-1)/12)));                 % Frekans fonksiyonu oluþturuldu.
-        end
-        
     end
-end                                                                         %sayýyý en yakýn deðere yuvarladýk yuvarlama kodu.
+
+end 
+   
+
+
