e94c7a2d376bcd759abd38bf4d5c62621a14e839
thehearsty14 <thehearsty14@hotmail.com>
2015-12-29 12:20:05 +0200

    note.m dosyası düzenlendi

--- note.m
+++ note.m
@@ -1,31 +1,16 @@
-function [x,t] = note(f,vurus)                                              %fonksiyonu tanimliyoruz.girilicek argumanlari tanýmladýk
-
-fs=8192;                                                                    %örnekleme frekansý
-
-t = 0:1/fs:vurus;                                                           %0 dan dur a kadar 1/(100*ff) kadar artýyor
-
-elemanS = length(tt);                                                       %% Eleman sayýsý
-
-
-    
-attack = linspace(0,1.5,(elemanS*2/8));                                     
-
-
-    
-decay = linspace(1.5-(0.5/(elemanS/8)),1,(elemanS/8));
-
-
-    
-sustain = ones(1,(elemanS/2));
-
-
-
-relase = linspace(1-(1/(elemanS/8)),0,(elemanS/8));
-
-
-    
-zarf = [attack, decay, sustain, relase];
-
-
-
-x = zarf .* sin(2*pi*f*t + zarf);                                           %% zarflý sinüs
+function[x,t]=note(frekans,vurus)                                           % sinüs sinyalini i çizdiriyoruz
+    fs=8192;                                                                %örnekleme sayýsý
+    t=0:1/fs:vurus-(1/fs);                                                  %periyot tanýmlandý
+       a=length(t);                                                         %a deðiþkenini tanýmladýk
+    hk=[1 0.8 0.4 0.1];                                                     
+        b=length(hk);                                                       %b deðiþkeni tanýmlandý
+        x=zeros(1,a);                                                       %x matrisi tanýmladýk
+        for i=1:b                                                           %1 den dizi boyutuna kadar bir deðiþken olusturuldu
+            x=x+(hk(i)*sin(2*pi*(i*frekans)*t));                            
+        end
+A=linspace(0,1.5,a/4);                                                      
+B=linspace(1.5,1,a/8);                                                     
+C=linspace(1,1,a/2);                                                        % vektörleri olusturduk
+D=linspace(1,0,a/8);                                                        
+ zarf=[A B C D];                                                            %vektörleri diziye aldýk
+ x=x.*zarf;
